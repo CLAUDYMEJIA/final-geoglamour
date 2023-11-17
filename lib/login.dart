c2d71511568284d028b7b78295d715c2c4f47e61
Lourdesmena <brenda.mena22@itca.edu.sv>
2023-11-17 15:04:04 -0600

    lourdes cambio1

--- lib/login.dart
+++ lib/login.dart
@@ -29,7 +29,7 @@ class LoginScreen extends StatelessWidget {
         backgroundColor: Colors.black,
         automaticallyImplyLeading: false,
         leading: Icon(Icons.person),
-        title: Text('seccion inicio'),
+        title: Text('login'),
       ),
       body: Container(
         color: Colors.white,
